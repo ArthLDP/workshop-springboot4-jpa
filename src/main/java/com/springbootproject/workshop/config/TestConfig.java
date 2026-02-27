5a2ec5c32be4fc8a41e4a1e1c281467ddbca52ea
ArthLDP <arthurlourencodp@gmail.com>
2026-02-27 14:19:36 -0300

    Add payment-order, one-to-one association

--- src/main/java/com/springbootproject/workshop/config/TestConfig.java
+++ src/main/java/com/springbootproject/workshop/config/TestConfig.java
@@ -44,9 +44,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "John", "john@email.com", "99999999", "123456");
         User user2 = new User(null, "Arthur", "arthur@email.com", "3333333", "123");
 
-        Order order1 = new Order(null, Instant.now(), OrderStatus.PAID, user1);
-        Order order2 = new Order(null, Instant.now(), OrderStatus.WAITING_PAYMENT, user2);
-        Order order3 = new Order(null, Instant.now(), OrderStatus.WAITING_PAYMENT, user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
@@ -67,6 +67,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem orderItem2 = new OrderItem(order1, p3, 1, p3.getPrice());
         OrderItem orderItem3 = new OrderItem(order2, p3, 2, p3.getPrice());
         OrderItem orderItem4 = new OrderItem(order3, p5, 2, p5.getPrice());
+
         orderItemRepository.saveAll(Arrays.asList(orderItem1, orderItem2, orderItem3, orderItem4));
+
+        Payment payment1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), order1);
+        order1.setPayment(payment1);
+
+        orderRepository.save(order1);
     }
 }
